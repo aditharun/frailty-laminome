048c3a178bf69633115182818af03623ee6c2505
Adith Arun <adith.3.arun@gmail.com>
2022-12-25 12:54:56 -0800

    stratification abundances
    add feature to output sheet of abundance differences

---
+++ code/get-strat-csv.R
@@ -0,0 +1,32 @@
+args = commandArgs(trailingOnly=TRUE)
+
+
+#volcano plots per tissue
+library(tidyverse)
+library(readxl)
+library(cowplot)
+library(writexl)
+
+dir <- "../results"
+
+sample <- args[1] %>% tolower()
+
+c.dir <- "../temp"
+
+files <- list.files(c.dir, pattern="*.diffexp", full.names=TRUE)
+
+df <- files[grepl(sample, files)] %>% read_excel()
+
+files.unfiltered <- list.files(c.dir, pattern="*.unfiltered", full.names=TRUE)
+
+df.unfiltered <- files.unfiltered[grepl(sample, files.unfiltered)] %>% read_excel()
+
+
+dat <- df.unfiltered %>% rename(pval=`p-value`) %>% mutate(type=ifelse(grepl("ribosomal|keratin|mitochondri", Protein), "mitochondrial/ribosomal/keratin\n(MRK)", "not MRK")) %>% mutate(diff=komean-wtmean) %>% group_by(type) %>% mutate(index=1:n()) %>% ungroup()
+
+res <- dat %>% select(Protein, diff, log2.foldchange, pval, komean, wtmean, type, sign) %>% arrange(desc(abs(diff)))
+
+pathtofile <- file.path("../results", paste0("strat-", sample, ".xlsx"))
+
+write_xlsx(res, path=pathtofile)
+
